df3960aad1238aa287db8f8b13a5e2304a92f195
vuthisak <khyvuthisak@gmail.com>
2024-10-11 22:58:03 +0700

    Updated text field and hint for create email

--- SampleSpotifyUi/app/src/main/java/com/example/sampleuispotify/features/CreateAccountEmailScreen.kt
+++ SampleSpotifyUi/app/src/main/java/com/example/sampleuispotify/features/CreateAccountEmailScreen.kt
@@ -6,12 +6,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -23,8 +25,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,8 +39,6 @@ import com.example.sampleuispotify.ui.theme.SampleUiSpotifyTheme
 fun CreateAccountEmailScreen(
     modifier: Modifier = Modifier
 ) {
-    var text by remember { mutableStateOf(TextFieldValue("")) }
-
     Scaffold(modifier = modifier) { paddingValues ->
         Column(
             modifier = Modifier
@@ -65,17 +66,32 @@ fun CreateAccountEmailScreen(
                 }, colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Transparent)
             )
 
+            val containerColor = Color(0xFF777777)
+            var text by remember { mutableStateOf("") }
 
-            TextField(
+            OutlinedTextField(
+                value = text,
+                onValueChange = { newText -> text = newText },
+                singleLine = true,
+                visualTransformation = VisualTransformation.None,
+                shape = RoundedCornerShape(5.dp),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = containerColor,
+                    unfocusedContainerColor = containerColor
+                ),
                 modifier = Modifier
                     .fillMaxWidth()
-                    .wrapContentHeight()
-                    .padding(horizontal = 16.dp),
-                value = text,
-                onValueChange = { text = it },
-                placeholder = {
-                    Text(text = "Email")
-                })
+                    .padding(start = 16.dp, end = 16.dp, top = 16.dp, bottom = 4.dp)
+                    .height(56.dp)
+            )
+
+            Text(
+                modifier = Modifier.padding(start = 16.dp),
+                text = "You’ll need to confirm this email later.",
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 8.sp,
+                style = TextStyle(color = Color.White)
+            )
         }
     }
 }
